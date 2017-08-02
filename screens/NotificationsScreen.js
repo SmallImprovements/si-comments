076993dac89b6169a27c7cabe7b10387ffed7f85
Lucas Arundell <larundell@small-improvements.com>
2017-08-02 09:18:38 +0200

    feat(NotificationsScreen) cleanup nav so that the jump to comments happens within the screen itself

--- screens/NotificationsScreen.js
+++ screens/NotificationsScreen.js
@@ -1,28 +1,59 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { Button, ScrollView, Text } from "react-native";
+import { StackNavigator } from "react-navigation";
+import Comment from "../components/Comment";
+import EntityPreview from "../components/EntityPreview";
+import { mockComments, mockObjective } from "../api/mockData";
 import { NotificationItem } from "../components/NotificationItem";
 import { notificationsLog } from "../api/mockData.js";
 
-export default class NotificationsScreen extends React.Component {
-  static navigationOptions = {
+const NotificationsList = ({ navigation }) =>
+  <ScrollView navigation={navigation} style={{ backgroundColor: "#fff" }}>
+    {notificationsLog.map(item =>
+      <NotificationItem
+        key={item.id}
+        text={item.type}
+        onSelect={() => {
+          navigation.navigate("CommentOverview", {
+            notification_id: item.entityId
+          });
+        }}
+      />
+    )}
+  </ScrollView>;
+
+NotificationsList.navigationOptions = ({ navigation, screenProps }) => {
+  return {
     title: "Notifications"
   };
+};
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {notificationsLog.map(item =>
-          <NotificationItem key={item.id} text={item.type} />
-        )}
-      </ScrollView>
-    );
-  }
-}
+const CommentOverview = ({ navigation }) =>
+  <ScrollView banner="Home Screen" navigation={navigation}>
+    <EntityPreview type="OBJECTIVE" entity={mockObjective} />
+    <Text>
+      {navigation.state.params.notification_id}
+    </Text>
+    {mockComments.map(comment => <Comment {...comment} key={comment.id} />)}
+  </ScrollView>;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
+CommentOverview.navigationOptions = ({ navigation, screenProps }) => {
+  return {
+    title: "Comment"
+  };
+};
+
+const CommentScreen = StackNavigator({
+  NotificationsList: {
+    screen: NotificationsList
+  },
+  CommentOverview: {
+    screen: CommentOverview
   }
 });
+
+CommentScreen.navigationOptions = {
+  header: null
+};
+
+export default CommentScreen;
