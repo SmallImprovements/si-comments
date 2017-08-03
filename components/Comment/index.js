b1740a1a5aa16a9fef8a0a8c476269f02de781f1
Lucas Arundell <larundell@small-improvements.com>
2017-08-03 17:20:11 +0200

    feat(Comment) separate name and avatar for better layout

--- components/Comment/index.js
+++ components/Comment/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 import { View, Text, TouchableHighlight } from "react-native";
 import styleVars from "../../assets/styles/vars";
-import Author from "../Author";
+import Avatar from "../Avatar";
 import Flex from "../Flex";
 
 const { standardPadding, commentBorder, commentBackgroundColour } = styleVars;
@@ -10,44 +10,39 @@ const { standardPadding, commentBorder, commentBackgroundColour } = styleVars;
 const CommentContainer = styled.View`
 	border-radius: 3px;
 	margin-right: 10px;
-	display: flex;
-	flex-wrap: wrap;
-	margin-top: -1px;
-	padding: ${standardPadding * 0.25}px;
+
+	flex-direction: row;
+	padding: ${standardPadding * 0.5}px 0;
 	border: ${commentBorder};
 	background-color: ${commentBackgroundColour};
-	width: 100%;
 `;
 
 const CommentContentContainer = styled.View`
-	padding-left: ${standardPadding * 2}px;
+	flex-shrink: 1;
+	padding: 0 ${standardPadding * 0.5}px;
 `;
+
+const LightGrayText = styled.Text`color: gray;`;
 
 const CommentText = styled.Text`margin-bottom: ${standardPadding * 0.5}px;`;
 
 export default function Comment({ content, author, num_likes_this }) {
+	const { imageSource, authorName } = author;
 	return (
 		<CommentContainer>
-			<Author {...author} />
+			<Avatar source={imageSource} />
 			<CommentContentContainer>
 				<CommentText>
+					<Text style={{ fontWeight: "bold" }}>
+						{authorName}
+					</Text>
 					{content}
 				</CommentText>
-				<Flex style={{ alignItems: "center" }}>
-					<Text>
-						{num_likes_this} Likes
-					</Text>
-					<TouchableHighlight
-						onPress={() => console.log("clicked Reply")}
-					>
-						<Text>Reply</Text>
-					</TouchableHighlight>
-					<TouchableHighlight
-						onPress={() => console.log("clicked Like")}
-					>
-						<Text>Like</Text>
-					</TouchableHighlight>
-				</Flex>
+				<TouchableHighlight
+					onPress={() => console.log("clicked Reply")}
+				>
+					<LightGrayText>Reply</LightGrayText>
+				</TouchableHighlight>
 			</CommentContentContainer>
 		</CommentContainer>
 	);
