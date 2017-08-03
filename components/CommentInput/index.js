b6c2285ecb5158084ba43256c8ace959b25e41d6
Lucas Arundell <larundell@small-improvements.com>
2017-08-03 11:37:47 +0200

    feat(CommentInput) create component

---
+++ components/CommentInput/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components/native";
+import { View, TextInput, Button } from "react-native";
+import styleVars from "../../assets/styles/vars";
+
+const { standardPadding } = styleVars;
+
+const CommentInputContainer = styled.View`
+  background: white;
+  borderWidth: 1px
+  borderColor: lightgray;
+  borderLeftWidth: 0;
+  borderRightWidth: 0;
+  flex-direction: row;
+  align-items: center;
+`;
+const CommentInputField = styled.TextInput`
+	padding: ${standardPadding * 0.75}px;
+	flex-grow: 1;
+`;
+
+export default function CommentInput() {
+	return (
+		<CommentInputContainer>
+			<CommentInputField placeholder="Write a comment..." />
+			<Button title="Post" onPress={() => console.log("pressed")} />
+		</CommentInputContainer>
+	);
+}
