be463a3f403590a740eacdffafcf1a5eda8d554e
Lucas Arundell <larundell@small-improvements.com>
2017-08-06 22:21:01 +0200

    feat(auth) add service, with some other options commented for now

---
+++ services/auth/factory.js
@@ -0,0 +1,137 @@
+import { forEach } from "lodash/fp";
+
+const BASE_URL = "http://localhost:8080";
+export default function auth(http) {
+    const state = {
+        user: null,
+        tokenProvider: null,
+        replacements: {}
+    };
+
+    // const authChangeListeners = [];
+
+    function getUser() {
+        return global.user;
+        // return state.user;
+    }
+
+    function setUser(user) {
+        // state.user = user;
+        global.user = user;
+        console.log(global.user);
+        return user;
+    }
+
+    // function getReplacements() {
+    //     return state.replacements;
+    // }
+
+    // function setReplacements(replacements) {
+    //     state.replacements = replacements;
+    //     return replacements;
+    // }
+
+    // function useTokenProvider(tokenProvider) {
+    //     state.tokenProvider = tokenProvider;
+    // }
+
+    function loginWithToken(token) {
+        setAuthorizationHeader(token);
+        return http.get("/api/v2/users/me").then(
+            res => {
+                const user = res.data;
+                // console.log(user);
+                setUser(user);
+                // return api.replacements.getGmailReplacements().then(replacements => {
+                //     setReplacements(replacements);
+                //     notifyAuthChangeListeners();
+                //     return { user, status: 'OK' };
+                // });
+            },
+            err => ({ err, status: "ERROR" })
+        );
+    }
+
+    // function tryLoginFromCache(email) {
+    //     const { tokenProvider } = state;
+    //     if (!tokenProvider || !tokenProvider.getStoredToken) {
+    //         throw new Error('No valid tokenProvider specified');
+    //     }
+
+    //     return tokenProvider.getStoredToken(email).then(token => {
+    //         return token ? loginWithToken(token) : { user: null, status: 'OK' };
+    //     });
+    // }
+
+    function login(USER_EMAIL) {
+        // const { tokenProvider } = state;
+        // if (!tokenProvider || !tokenProvider.requestToken) {
+        //     throw new Error("No valid tokenProvider specified");
+        // }
+        http.defaults.baseURL = BASE_URL;
+
+        return requestToken(USER_EMAIL)
+            .then(loginWithToken)
+            .catch(err => ({ err, status: "ERROR" }));
+
+        // return loginWithToken;
+    }
+
+    function requestToken(USER_EMAIL) {
+        return http
+            .get("/api/external-services/token-dev", {
+                params: { loginName: USER_EMAIL }
+            })
+            .then(res => res.data.access_token);
+    }
+
+    // function logout() {
+    //     const { tokenProvider, user } = state;
+
+    //     setUser(null);
+    //     setReplacements({});
+    //     setAuthorizationHeader(null);
+    //     notifyAuthChangeListeners();
+
+    //     if (!tokenProvider || !tokenProvider.removeToken) {
+    //         throw new Error('No valid tokenProvider specified');
+    //     }
+    //     return tokenProvider.removeToken(user.email);
+    // }
+
+    function isLoggedIn() {
+        return !!global.user;
+        // return !!state.user;
+    }
+
+    // function onAuthChange(cb) {
+    //     authChangeListeners.push(cb);
+    //     return () => {
+    //         const i = authChangeListeners.indexOf(cb);
+    //         if (i !== -1) {
+    //             authChangeListeners.splice(i, 1);
+    //         }
+    //     };
+    // }
+
+    // function notifyAuthChangeListeners() {
+    //     forEach(listener => listener(getUser(), getReplacements()), authChangeListeners);
+    // }
+
+    function setAuthorizationHeader(token) {
+        const bearer = token ? `Bearer ${token}` : null;
+        http.defaults.headers.common.Authorization = bearer;
+    }
+
+    return {
+        // tryLoginFromCache,
+        login,
+        // logout,
+        isLoggedIn,
+        getUser,
+        // getReplacements,
+        setUser
+        // onAuthChange,
+        // useTokenProvider
+    };
+}
