3bece5bdfcaff09f432d30aa936ad8caa325f999
Lucas Arundell <larundell@small-improvements.com>
2017-08-08 15:21:47 +0200

    feat(Login) add header components

--- features/Login/index.js
+++ features/Login/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { View, Text, Button, TextInput } from "react-native";
 import StyledInput from "../../components/StyledInput";
-
+import { HeaderOne } from "../../components/Text";
 import auth from "../../services/auth";
 
-const BASE_URL = "http://localhost:8080";
 const USER_EMAIL = "demo@example.com";
 
 export default class Login extends Component {
@@ -18,27 +17,32 @@ export default class Login extends Component {
 		const { currentUser } = this.props;
 		const { email } = this.state;
 		const login = () => auth.login(email);
-		const logout = () => auth.logout();
 
 		const onChange = ev => this.setState({ email: ev.target.value });
 		return (
-			<View>
-				<Text>Mock Login</Text>
-
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					height: "100%"
+				}}
+			>
+				<HeaderOne style={{ textAlign: "center", marginBottom: 20 }}>
+					Please Log In
+				</HeaderOne>
 				<View>
-					<Text>Email Address</Text>
 					<View>
 						<StyledInput
 							onChange={onChange}
 							value={email}
 							placeholder="Your email"
+							style={{ marginBottom: 20 }}
 						/>
 					</View>
 				</View>
 
 				<View>
 					<Button onPress={login} title="Login" />
-					<Button onPress={logout} title="Logout" />
 				</View>
 				{!!currentUser &&
 					<Text>
