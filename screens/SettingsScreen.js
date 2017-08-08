9820629affb496443e4cb84ab87d22914628ca19
Lucas Arundell <larundell@small-improvements.com>
2017-08-08 14:18:21 +0200

    feat(SettingsScreen) add logout button

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { ExpoConfigView } from "@expo/samples";
+import { View, Text, Button } from "react-native";
+import auth from "../services/auth";
 
 export default class SettingsScreen extends React.Component {
 	static navigationOptions = {
@@ -9,6 +11,13 @@ export default class SettingsScreen extends React.Component {
 	render() {
 		/* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
-		return <ExpoConfigView />;
+		const logout = () => auth.logout();
+		return (
+			<View>
+				<Text>Settings</Text>
+				<Button title="Logout" onPress={logout} />
+				<ExpoConfigView />
+			</View>
+		);
 	}
 }
