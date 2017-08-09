c73ffd049c99a76b9295efc23e5a661fd9daae28
Lucas Arundell <larundell@small-improvements.com>
2017-08-09 09:20:21 +0200

    feat(Login) add logging in state, return to form if error

--- features/Login/index.js
+++ features/Login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput } from 'react-native';
+import { View, Text, Button, TextInput, ActivityIndicator } from 'react-native';
 import StyledInput from '../../components/StyledInput';
 import { HeaderOne } from '../../components/Text';
 import auth from '../../services/auth';
@@ -11,12 +11,18 @@ export default class Login extends Component {
         super(props);
         this.state = {
             email: USER_EMAIL,
+            isLoggingIn: false,
         };
+        this.logUserIn = this.logUserIn.bind(this);
+    }
+    logUserIn() {
+        this.setState({ isLoggingIn: true });
+        auth.login(this.state.email).then(err => this.setState({ isLoggingIn: false }));
     }
     render() {
         const { currentUser } = this.props;
-        const { email } = this.state;
-        const login = () => auth.login(email);
+        const { email, isLoggingIn } = this.state;
+        // const login = () => auth.login(email);
 
         const onChange = ev => this.setState({ email: ev.target.value });
         return (
@@ -27,25 +33,32 @@ export default class Login extends Component {
                     height: '100%',
                 }}
             >
-                <HeaderOne style={{ textAlign: 'center', marginBottom: 20 }}>Please Log In</HeaderOne>
-                <View>
-                    <View>
-                        <StyledInput
-                            onChange={onChange}
-                            value={email}
-                            placeholder="Your email"
-                            style={{ marginBottom: 20 }}
-                        />
-                    </View>
-                </View>
+                {isLoggingIn
+                    ? <View>
+                          <ActivityIndicator />
+                          <Text style={{ textAlign: 'center', marginTop: 20 }}>Logging In...</Text>
+                      </View>
+                    : <View>
+                          <HeaderOne style={{ textAlign: 'center', marginBottom: 20 }}>Please Log In</HeaderOne>
+                          <View>
+                              <View>
+                                  <StyledInput
+                                      onChange={onChange}
+                                      value={email}
+                                      placeholder="Your email"
+                                      style={{ marginBottom: 20 }}
+                                  />
+                              </View>
+                          </View>
 
-                <View>
-                    <Button onPress={login} title="Login" />
-                </View>
-                {!!currentUser &&
-                    <Text>
-                        Currently logged in as {currentUser.name}
-                    </Text>}
+                          <View>
+                              <Button onPress={this.logUserIn} title="Login" />
+                          </View>
+                          {!!currentUser &&
+                              <Text>
+                                  Currently logged in as {currentUser.name}
+                              </Text>}
+                      </View>}
             </View>
         );
     }
