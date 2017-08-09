057747c56fcbc6c12324e5a3f831981049f54a4f
Lucas Arundell <larundell@small-improvements.com>
2017-08-09 10:47:17 +0200

    feat(NotificationsList) use ladda withData, wrire it up

--- features/NotificationsList/index.js
+++ features/NotificationsList/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
-import { NotificationItem } from '../../components/NotificationItem';
-import { notificationsLog } from '../../api/mockData.js';
+import { withData } from 'ladda-react';
+import NotificationsList from './presenter';
+import { getNotifications } from '../../services/api';
 
-export default function NotificationsList({ navigation }) {
-    return (
-        <ScrollView navigation={navigation} style={{ backgroundColor: '#fff' }}>
-            {notificationsLog.map(item =>
-                <NotificationItem
-                    key={item.id}
-                    text={item.type}
-                    onSelect={() => {
-                        navigation.navigate('CommentsList', {
-                            notification_id: item.entityId,
-                        });
-                    }}
-                />
-            )}
-        </ScrollView>
-    );
-}
-
-NotificationsList.navigationOptions = ({ navigation, screenProps }) => {
-    return {
-        title: 'Notifications',
-    };
-};
+export default withData({
+	resolve: {
+		notifications: getNotifications(),
+	},
+})(NotificationsList);
