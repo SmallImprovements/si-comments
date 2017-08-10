8d6d9ecc3ab67f5f121cf76040967f979b3c9bdb
Lucas Arundell <larundell@small-improvements.com>
2017-08-10 08:28:36 +0200

    feat(notificationsList) properly pass getter function to withData

--- features/NotificationsList/index.js
+++ features/NotificationsList/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { withData } from 'ladda-react';
 import NotificationsList from './presenter';
 import { getNotifications } from '../../services/api';
+import { ActivityIndicator } from 'react-native';
 
 export default withData({
-	resolve: {
-		notifications: getNotifications(),
-	},
+  resolve: {
+    notifications: () => getNotifications(),
+  },
+  pendingComponent: () => <ActivityIndicator />,
 })(NotificationsList);
