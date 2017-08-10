528bc32a98589b52b678e4367f2617ee19c1d605
Lucas Arundell <larundell@small-improvements.com>
2017-08-10 17:23:59 +0200

    feat(Notifications) filter only to show comment-related notifications

--- features/NotificationsList/presenter.js
+++ features/NotificationsList/presenter.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { NotificationItem } from '../../components/NotificationItem';
-// import { notificationsLog } from '../../api/mockData.js';
 import { getNotifications } from '../../services/api';
+import { filterNotifications } from '../../services/notifications';
 
 export default function NotificationsList({ navigation, notifications }) {
+    const filteredNotifications = filterNotifications(notifications);
     return (
         <ScrollView navigation={navigation} style={{ backgroundColor: '#fff' }}>
-            {notifications.map(item =>
-                <NotificationItem
-                    key={item.id}
-                    text={item.type}
-                    onSelect={() => {
-                        navigation.navigate('CommentsList', {
-                            notification_id: item.entityId,
-                        });
-                    }}
-                />
-            )}
+            {filteredNotifications.length
+                ? filteredNotifications.map(item =>
+                      <NotificationItem
+                          key={item.id}
+                          text={item.type}
+                          onSelect={() => {
+                              navigation.navigate('CommentsList', {
+                                  notification_id: item.entityId,
+                              });
+                          }}
+                      />
+                  )
+                : <Text>You have no Notifications</Text>}
         </ScrollView>
     );
 }
