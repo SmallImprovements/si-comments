34f40cc19923efb99ebb8407fed042780a907fd1
Lucas Arundell <larundell@small-improvements.com>
2017-08-11 16:53:52 +0200

    feat(login) fix incorrect bindings of email value. Properly handle login errors with Alert

--- features/Login/index.js
+++ features/Login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, ActivityIndicator } from 'react-native';
+import { View, Text, Button, TextInput, ActivityIndicator, Alert } from 'react-native';
 import StyledInput from '../../components/StyledInput';
 import { HeaderOne } from '../../components/Text';
 import auth from '../../services/auth';
@@ -7,61 +7,74 @@ import auth from '../../services/auth';
 const USER_EMAIL = 'demo@example.com';
 
 export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: USER_EMAIL,
-            isLoggingIn: false,
-        };
-        this.logUserIn = this.logUserIn.bind(this);
-    }
-    logUserIn() {
-        this.setState({ isLoggingIn: true });
-        auth.login(this.state.email).then(err => {
-            this.setState({ isLoggingIn: false });
-        });
-    }
-    render() {
-        const { currentUser } = this.props;
-        const { email, isLoggingIn } = this.state;
-        // const login = () => auth.login(email);
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: USER_EMAIL,
+      isLoggingIn: false,
+    };
+    this.logUserIn = this.logUserIn.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  logUserIn() {
+    const { email } = this.state;
 
-        const onChange = ev => this.setState({ email: ev.target.value });
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    height: '100%',
-                }}
-            >
-                {isLoggingIn
-                    ? <View>
-                          <ActivityIndicator />
-                          <Text style={{ textAlign: 'center', marginTop: 20 }}>Logging In...</Text>
-                      </View>
-                    : <View>
-                          <HeaderOne style={{ textAlign: 'center', marginBottom: 20 }}>Please Log In</HeaderOne>
-                          <View>
-                              <View>
-                                  <StyledInput
-                                      onChange={onChange}
-                                      value={email}
-                                      placeholder="Your email"
-                                      style={{ marginBottom: 20 }}
-                                  />
-                              </View>
-                          </View>
+    this.setState({ isLoggingIn: true });
 
-                          <View>
-                              <Button onPress={this.logUserIn} title="Login" />
-                          </View>
-                          {!!currentUser &&
-                              <Text>
-                                  Currently logged in as {currentUser.name}
-                              </Text>}
-                      </View>}
+    auth.login(this.state.email).catch(err => {
+      const { message } = err;
+      this.setState({ isLoggingIn: false });
+      this.showErrorAlert(message);
+    });
+  }
+
+  handleInputChange(value) {
+    this.setState({ email: value });
+  }
+
+  showErrorAlert(message) {
+    Alert.alert(
+      "Can't Log In",
+      `Either your email address or password is incorrect. ${message}`,
+      [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+      { cancelable: true }
+    );
+  }
+  render() {
+    const { currentUser } = this.props;
+    const { email, isLoggingIn, loginError } = this.state;
+    const { logUserIn, handleInputChange } = this;
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          height: '100%',
+        }}
+      >
+        {isLoggingIn
+          ? <View>
+              <ActivityIndicator />
+              <Text style={{ textAlign: 'center', marginTop: 20 }}>Logging In...</Text>
             </View>
-        );
-    }
+          : <View>
+              <HeaderOne style={{ textAlign: 'center', marginBottom: 20 }}>Please Log In</HeaderOne>
+              <View>
+                <View>
+                  <StyledInput
+                    onChangeText={this.handleInputChange}
+                    value={email}
+                    placeholder="Your email"
+                    style={{ marginBottom: 20 }}
+                  />
+                </View>
+              </View>
+
+              <View>
+                <Button onPress={logUserIn} title="Login" />
+              </View>
+            </View>}
+      </View>
+    );
+  }
 }
