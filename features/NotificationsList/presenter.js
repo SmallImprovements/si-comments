e443ba29386d760108f150e48d4691dcce0f1467
Lucas Arundell <larundell@small-improvements.com>
2017-08-12 17:49:32 +0200

    feat(NotificationsList) replace with proper ListView component

--- features/NotificationsList/presenter.js
+++ features/NotificationsList/presenter.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import React, { Component } from 'react';
+import { ListView, Text } from 'react-native';
 import { NotificationItem } from '../../components/NotificationItem';
-import { getNotifications } from '../../services/api';
 import { filterNotifications } from '../../services/notifications';
 
-export default function NotificationsList({ navigation, notifications }) {
-    const filteredNotifications = filterNotifications(notifications);
-    return (
-        <ScrollView navigation={navigation} style={{ backgroundColor: '#fff' }}>
-            {filteredNotifications.length
-                ? filteredNotifications.map(item =>
-                      <NotificationItem
-                          key={item.id}
-                          text={item.type}
-                          onSelect={() => {
-                              navigation.navigate('CommentsList', {
-                                  notification_id: item.entityId,
-                              });
-                          }}
-                      />
-                  )
-                : <Text>You have no Notifications</Text>}
-        </ScrollView>
-    );
+export default class NotificationsList extends Component {
+    constructor(props) {
+        super(props);
+        const { notifications } = this.props;
+        const filteredNotifications = filterNotifications(notifications);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            dataSource: ds.cloneWithRows(filteredNotifications),
+        };
+    }
+    render() {
+        const { navigation } = this.props;
+        return (
+            <ListView
+                navigation={navigation}
+                dataSource={this.state.dataSource}
+                style={{ backgroundColor: '#fff' }}
+                renderRow={rowData =>
+                    <NotificationItem
+                        key={rowData.id}
+                        text={rowData.type}
+                        onSelect={() => {
+                            navigation.navigate('CommentsList', {
+                                notification_id: rowData.entityId,
+                            });
+                        }}
+                    />}
+            />
+        );
+    }
 }
 
 NotificationsList.navigationOptions = ({ navigation, screenProps }) => {
