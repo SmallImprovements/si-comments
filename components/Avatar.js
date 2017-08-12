15c6e069f4d83b10b0d4780631687f8f4ac36e4f
Lucas Arundell <larundell@small-improvements.com>
2017-08-12 23:58:15 +0200

    feat(Avatar) allow for svg support

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Image } from 'react-native';
+import Svg from './Svg';
+import { View, Image } from 'react-native';
+import { endsWith } from 'lodash';
 
 const AVATAR_SIZE = 30;
+const BASE_URL = 'http://localhost:8080';
 
-export default (Avatar = styled.Image`
-    border-radius: ${AVATAR_SIZE / 2}px;
-    width: ${AVATAR_SIZE}px;
-    height: ${AVATAR_SIZE}px;
-`);
+const AvatarContainer = styled.View`
+	border-radius: ${AVATAR_SIZE / 2}px;
+	width: ${AVATAR_SIZE}px;
+	height: ${AVATAR_SIZE}px;
+	overflow: hidden;
+`;
+
+const StyledImage = styled.Image`
+	width: ${AVATAR_SIZE}px;
+	height: ${AVATAR_SIZE}px;
+`;
+
+// Svg does not play nicely with Styled-Components, so pass in style as a normal object
+const SvgStyle = {
+	width: AVATAR_SIZE,
+	height: AVATAR_SIZE,
+};
+
+export default function Avatar(props) {
+	const { logoUrl } = props;
+
+	const avatarSource = {
+		uri: `${BASE_URL}${logoUrl}`,
+	};
+	return (
+		<AvatarContainer>
+			{endsWith(logoUrl, 'svg')
+				? <Svg style={SvgStyle} source={avatarSource} />
+				: <StyledImage source={avatarSource} {...props} />}
+		</AvatarContainer>
+	);
+}
