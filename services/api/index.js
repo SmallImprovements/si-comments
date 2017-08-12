f0560c70b952fead81ba76cc5990b550d3000e9e
Lucas Arundell <larundell@small-improvements.com>
2017-08-12 20:49:33 +0200

    feat(Api) add getObjective api call

--- services/api/index.js
+++ services/api/index.js
@@ -5,3 +5,7 @@ const BASE_URL = '/api/v2';
 export function getNotifications() {
 	return http.get(`${BASE_URL}/notificationLogs/latest`).then(res => res.data, err => ({ err, status: 'ERROR' }));
 }
+
+export function getObjectiveById(id) {
+	return http.get(`${BASE_URL}/objective-preview/${id}`).then(res => res.data, err => ({ err, status: 'ERROR' }));
+}
