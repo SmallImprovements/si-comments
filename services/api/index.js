eb1ebb1b47dc86f520bf34486b6e4f7741d3a964
Lucas Arundell <larundell@small-improvements.com>
2017-08-12 23:56:57 +0200

    feat(api) add comment getters

--- services/api/index.js
+++ services/api/index.js
@@ -12,6 +12,32 @@ export function getObjectiveById(objectiveId) {
 		.then(res => res.data, err => ({ err, status: 'ERROR' }));
 }
 
+export function getObjectiveComments(objectiveId) {
+	return http
+		.get(`${BASE_URL}/objectives/${objectiveId}/messages`)
+		.then(res => res.data, err => ({ err, status: 'ERROR' }));
+}
+
 export function getPraiseById(praiseId) {
 	return http.get(`${BASE_URL}/praise/${praiseId}`).then(res => res.data, err => ({ err, status: 'ERROR' }));
+}
+
+export function getPraiseComments(praiseId) {
+	return http
+		.get(`${BASE_URL}/praise/${praiseId}/comments?fields=$message.list`)
+		.then(res => res.data, err => ({ err, status: 'ERROR' }));
+}
+
+export function getComments(entityId, moduleType) {
+	console.log(moduleType);
+	switch (moduleType) {
+		case 'OBJECTIVE':
+			return getObjectiveComments(entityId);
+		case 'MESSAGE':
+			return getPraiseComments(entityId);
+		case 'PRAISE':
+			return getPraiseComments(entityId);
+		default:
+			throw new Error('No valid  moduleType provided');
+	}
 }
