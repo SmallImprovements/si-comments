44451898944d0fec920608040d784c867b88c49d
Lucas Arundell <larundell@small-improvements.com>
2017-08-13 14:25:44 +0200

    feat(api) add comment create calls

--- services/api/index.js
+++ services/api/index.js
@@ -29,7 +29,6 @@ export function getPraiseComments(praiseId) {
 }
 
 export function getComments(entityId, moduleType) {
-	console.log(moduleType);
 	switch (moduleType) {
 		case 'OBJECTIVE':
 			return getObjectiveComments(entityId);
@@ -39,5 +38,61 @@ export function getComments(entityId, moduleType) {
 			return getPraiseComments(entityId);
 		default:
 			throw new Error('No valid  moduleType provided');
+	}
+}
+
+export function postPraiseComment(praiseId, data) {
+	console.log(praiseId, data);
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	const dto = {
+		masterMessageId: praiseId,
+		message: data.comment,
+		title: 'Re: sdfdf',
+		visibleTo: [],
+	};
+	return http
+		.post(`${BASE_URL}/praise/${praiseId}/comments`, dto, config)
+		.then(res => res.data, err => ({ err, status: 'ERROR' }));
+}
+
+export function postObjectiveComment(objectiveId, data) {
+	console.log(objectiveId, data);
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+	const dto = {
+		masterMessageId: objectiveId,
+		message: data,
+		title: 'Re: sdfdf',
+		visibleTo: [],
+	};
+	// "/objective-cycles/{cycleId}/objectives/{id}/messages"
+	return http.post(`${BASE_URL}/objective-cycles/${cycleId}/objectives/${objectiveId}/messages`, dto, config).then(
+		res => {
+			// console.log(res);
+			return res.data;
+		},
+		err => {
+			console.log(err);
+		}
+	);
+}
+
+export function postComment({ moduleType, entityId, data }) {
+	switch (moduleType) {
+		case 'OBJECTIVE':
+			return postObjectiveComment(entityId, data);
+		case 'MESSAGE':
+			return postPraiseComment(entityId, data);
+		case 'PRAISE':
+			return postPraiseComment(entityId, data);
+		default:
+			throw new Error('No valid  moduleType provided to postComment');
 	}
 }
