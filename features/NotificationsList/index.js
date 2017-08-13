15b4cef657f1ff9d9ed4a96dc9c1b1a4d40a9aad
Lucas Arundell <larundell@small-improvements.com>
2017-08-13 22:18:55 +0200

    refactor(NotificationsList) remove withData, handle state internally. Add pull to refresh control

--- features/NotificationsList/index.js
+++ features/NotificationsList/index.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import { withData } from 'ladda-react';
-import NotificationsList from './presenter';
+import React, { Component } from 'react';
+import { View, ListView, Text, RefreshControl, ActivityIndicator } from 'react-native';
+import { NotificationItem } from '../../components/NotificationItem';
+import { filterNotifications } from '../../services/notifications';
 import { getNotifications } from '../../services/api';
-import { ActivityIndicator } from 'react-native';
+export default class NotificationsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            refreshing: false,
+            dataSource: null,
+        };
 
-export default withData({
-  resolve: {
-    notifications: getNotifications,
-  },
-  pendingComponent: () => <ActivityIndicator />,
-})(NotificationsList);
+        this.onSelect = this.onSelect.bind(this);
+    }
+
+    doGetNotifications() {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        return getNotifications().then(res => {
+            this.setState({ dataSource: ds.cloneWithRows(this.filterTheNotifications(res)) });
+        });
+    }
+
+    filterTheNotifications(notifications) {
+        return filterNotifications(notifications);
+    }
+
+    onRefresh() {
+        this.setState({ refreshing: true });
+        this.doGetNotifications().then(() => {
+            this.setState({ refreshing: false });
+        });
+    }
+
+    onSelect(navigation, rowData) {
+        navigation.navigate('EntityCommentView', {
+            moduleType: rowData.module,
+            entityId: rowData.entityId,
+        });
+    }
+
+    componentDidMount() {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        return {
+            dataSource: ds.cloneWithRows(this.doGetNotifications()),
+        };
+    }
+    render() {
+        const { navigation } = this.props;
+        const { dataSource } = this.state;
+        return (
+            <View>
+                {dataSource
+                    ? <ListView
+                          navigation={navigation}
+                          dataSource={this.state.dataSource}
+                          style={{ backgroundColor: '#fff', height: '100%' }}
+                          renderRow={rowData => {
+                              return (
+                                  <NotificationItem
+                                      key={rowData.id}
+                                      text={rowData.type}
+                                      onSelect={() => this.onSelect(navigation, rowData)}
+                                  />
+                              );
+                          }}
+                          refreshControl={
+                              <RefreshControl
+                                  refreshing={this.state.refreshing}
+                                  onRefresh={this.onRefresh.bind(this)}
+                              />
+                          }
+                      />
+                    : <ActivityIndicator />}
+            </View>
+        );
+    }
+}
+
+NotificationsList.navigationOptions = ({ navigation, screenProps }) => {
+    return {
+        title: 'Notifications',
+    };
+};
