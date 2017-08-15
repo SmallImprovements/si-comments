eb3c616c26b87f72ce4c3e04dcc1d950966fb21a
Lucas Arundell <larundell@small-improvements.com>
2017-08-15 08:19:48 +0200

    feat(CommentList) - refresh comments after submitting

--- features/CommentsList/presenter.js
+++ features/CommentsList/presenter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import Comment from '../../components/Comment';
+import { ActivityIndicator } from 'react-native';
 export default function CommentsList({ comments, moduleType }) {
 	return (
 		<View>
 			{comments.length
 				? comments.map(comment => <Comment {...comment} key={comment.id} moduleType={moduleType} />)
-				: null}
+				: <ActivityIndicator />}
 		</View>
 	);
 }
