adeec37d632cb16d68ff80608af8a2e9d771fdad
Lucas Arundell <larundell@small-improvements.com>
2017-08-20 21:42:52 +0200

    feat(ModuleIcon) prettier

--- components/ModuleIcon/index.js
+++ components/ModuleIcon/index.js
@@ -6,22 +6,22 @@ import { Image } from 'react-native';
 import styled from 'styled-components/native';
 
 const ICON_BY_TYPE = {
-    MESSAGE: require('./png/module-messages-icon.png'),
-    PRAISE: require('./png/module-praise-icon.png'),
-    OBJECTIVE: require('./png/module-objectives-icon.png'),
+	MESSAGE: require('./png/module-messages-icon.png'),
+	PRAISE: require('./png/module-praise-icon.png'),
+	OBJECTIVE: require('./png/module-objectives-icon.png'),
 };
 
 ModuleIcon.propTypes = {
-    type: PropTypes.oneOf(keys(ICON_BY_TYPE)).isRequired,
+	type: PropTypes.oneOf(keys(ICON_BY_TYPE)).isRequired,
 };
 
 const StyledImage = styled.Image`
-    width: 15px;
-    height: 15px;
-    opacity: 0.4;
+	width: 15px;
+	height: 15px;
+	opacity: 0.4;
 `;
 
 export default function ModuleIcon({ type }) {
-    const icon = ICON_BY_TYPE[type] || '';
-    return <StyledImage source={icon} />;
+	const icon = ICON_BY_TYPE[type] || '';
+	return <StyledImage source={icon} />;
 }
