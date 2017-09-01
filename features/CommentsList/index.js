04d7f5f6a7cccfe8094deec708a99f63cc2923bb
Lucas Arundell <larundell@small-improvements.com>
2017-09-01 12:02:50 +0200

    feat(CommentsList) style no comnents

--- features/CommentsList/index.js
+++ features/CommentsList/index.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import Comment from '../../components/Comment';
 import { ActivityIndicator } from 'react-native';
 import { reverse } from 'lodash';
+import styled from 'styled-components/native';
+import colorVars from '../../assets/styles/colours';
+
 export default function CommentsList({ comments, moduleType, onPressReply, inputRef }) {
 	let sortedComments = comments;
 	if (moduleType === 'OBJECTIVE') {
 		sortedComments = reverse(sortedComments);
 	}
-	return (
-		<View>
-			{sortedComments.length
-				? sortedComments.map(comment =>
-						<Comment
-							{...comment}
-							key={comment.id}
-							moduleType={moduleType}
-							onPressReply={onPressReply}
-							inputRef={inputRef}
-						/>
-					)
-				: <ActivityIndicator />}
-		</View>
-	);
+
+	if (!sortedComments) {
+		return <ActivityIndicator />;
+	}
+	return !sortedComments
+		? <ActivityIndicator />
+		: <View>
+				{sortedComments.length
+					? sortedComments.map(comment =>
+							<Comment
+								{...comment}
+								key={comment.id}
+								moduleType={moduleType}
+								onPressReply={onPressReply}
+								inputRef={inputRef}
+							/>
+						)
+					: <View>
+							<NoComments>No Comments</NoComments>
+							<Button onPress={onPressReply} title="Write a comment..." />
+						</View>}
+			</View>;
 }
+
+const NoComments = styled.Text`
+	text-align: center;
+	margin: 40px 0;
+	color: ${colorVars.SIGray2};
+`;
