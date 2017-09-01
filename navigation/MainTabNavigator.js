bf409e2c09614062f8c8994acbed6a5fcda73528
Lucas Arundell <larundell@small-improvements.com>
2017-09-01 12:04:22 +0200

    feat(MainTabNavigator) prettier

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -18,30 +18,32 @@ export default TabNavigator(
         },
     },
     {
-        navigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused }) => {
-                const { routeName } = navigation.state;
-                let iconName;
-                switch (routeName) {
-                    case 'Notifications':
-                        iconName =
-                            Platform.OS === 'ios'
-                                ? `ios-notifications${focused ? '' : '-outline'}`
-                                : 'md-notifications-none';
-                        break;
-                    case 'Settings':
-                        iconName = Platform.OS === 'ios' ? `ios-contact${focused ? '' : '-outline'}` : 'md-options';
-                }
-                return (
-                    <Ionicons
-                        name={iconName}
-                        size={28}
-                        style={{ marginBottom: -3 }}
-                        color={focused ? SIBlue : SIGray4}
-                    />
-                );
-            },
-        }),
+        navigationOptions: ({ navigation }) => {
+            return {
+                tabBarIcon: ({ focused }) => {
+                    const { routeName } = navigation.state;
+                    let iconName;
+                    switch (routeName) {
+                        case 'Notifications':
+                            iconName =
+                                Platform.OS === 'ios'
+                                    ? `ios-notifications${focused ? '' : '-outline'}`
+                                    : 'md-notifications-none';
+                            break;
+                        case 'Settings':
+                            iconName = Platform.OS === 'ios' ? `ios-contact${focused ? '' : '-outline'}` : 'md-options';
+                    }
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            size={28}
+                            style={{ marginBottom: -3 }}
+                            color={focused ? SIBlue : SIGray4}
+                        />
+                    );
+                },
+            };
+        },
         tabBarComponent: TabBarBottom,
         tabBarPosition: 'bottom',
         animationEnabled: false,
