380836dbc266e9c7fa213a3fec3bc993f3b9fc1e
Lucas Arundell <larundell@small-improvements.com>
2017-09-02 22:04:36 +0200

    feat(Settings) add info about logged in user

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
-import { View, Text, Button } from 'react-native';
+// import { ExpoConfigView } from '@expo/samples';
+import { View, Text, Button, AsyncStorage } from 'react-native';
 import auth from '../services/auth';
 import Avatar from '../components/Avatar';
+import { HeaderOne } from '../components/Text';
 
 export default class SettingsScreen extends React.Component {
     static navigationOptions = {
@@ -11,26 +12,41 @@ export default class SettingsScreen extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            userToken: null,
+        };
+    }
+
+    componentWillMount() {
+        AsyncStorage.getItem('userToken')
+            .then(value => {
+                this.setState({ userToken: value });
+            })
+            .done();
     }
 
     render() {
-        /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-        console.log(this.props);
         const { currentUser } = this.props.screenProps;
         const { name, logo } = currentUser;
+        const { userToken } = this.state;
         const logout = () => auth.logout();
+        const clearAllFromLocalDB = () => auth.clearAllFromLocalDB();
         return (
-            <View>
-                <Text>
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+                <HeaderOne>
                     Logged in as{' '}
                     <Text>
                         <Avatar logoUrl={logo} />
                         {name}
                     </Text>
-                </Text>
-                <Button title="Logout" onPress={logout} />
-                <ExpoConfigView />
+                </HeaderOne>
+                <HeaderOne style={{ color: userToken ? 'black' : 'red' }}>
+                    Token is {userToken || 'not set!'}
+                </HeaderOne>
+                <View>
+                    <Button title="Logout" onPress={logout} />
+                    <Button title="Clear all Local Storage" onPress={clearAllFromLocalDB} />
+                </View>
             </View>
         );
     }
