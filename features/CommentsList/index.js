627cc83aae9a091ea7615a41e2b0fdbfab2ab667
Lucas Arundell <larundell@small-improvements.com>
2017-09-10 17:31:13 +0200

    feat(CommentsList) use FlatList

--- features/CommentsList/index.js
+++ features/CommentsList/index.js
@@ -1,42 +1,49 @@
 import React from 'react';
 import { View, Button } from 'react-native';
 import Comment from '../../components/Comment';
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator, FlatList } from 'react-native';
 import { reverse } from 'lodash';
 import styled from 'styled-components/native';
 import colorVars from '../../assets/styles/colours';
 
 export default function CommentsList({ comments, moduleType, onPressReply, inputRef }) {
-	let sortedComments = comments;
-	if (moduleType === 'OBJECTIVE') {
-		sortedComments = reverse(sortedComments);
-	}
+    let dataSource = comments;
+    if (moduleType === 'OBJECTIVE') {
+        dataSource = reverse(dataSource);
+    }
 
-	if (!sortedComments) {
-		return <ActivityIndicator />;
-	}
-	return !sortedComments
-		? <ActivityIndicator />
-		: <View>
-				{sortedComments.length
-					? sortedComments.map(comment =>
-							<Comment
-								{...comment}
-								key={comment.id}
-								moduleType={moduleType}
-								onPressReply={onPressReply}
-								inputRef={inputRef}
-							/>
-						)
-					: <View>
-							<NoComments>No Comments</NoComments>
-							<Button onPress={onPressReply} title="Write a comment..." />
-						</View>}
-			</View>;
+    if (!dataSource) {
+        return <ActivityIndicator />;
+    }
+    return (
+        <View>
+            {dataSource.length ? (
+                <FlatList
+                    data={dataSource}
+                    renderItem={({ item }) => {
+                        return (
+                            <Comment
+                                {...item}
+                                moduleType={moduleType}
+                                onPressReply={onPressReply}
+                                inputRef={inputRef}
+                            />
+                        );
+                    }}
+                    keyExtractor={comment => comment.id}
+                />
+            ) : (
+                <View>
+                    <NoComments>No Comments</NoComments>
+                    <Button onPress={onPressReply} title="Write a comment..." />
+                </View>
+            )}
+        </View>
+    );
 }
 
 const NoComments = styled.Text`
-	text-align: center;
-	margin: 40px 0;
-	color: ${colorVars.SIGray2};
+    text-align: center;
+    margin: 40px 0;
+    color: ${colorVars.SIGray2};
 `;
