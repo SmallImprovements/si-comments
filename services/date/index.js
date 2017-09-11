90d52370a51117b9eb5ac59da29e5d1c705e2fbc
Lucas Arundell <larundell@small-improvements.com>
2017-09-11 15:20:36 +0200

    feat(date service) add ‘ago’ to string

--- services/date/index.js
+++ services/date/index.js
@@ -1,14 +1,14 @@
 import moment from 'moment';
 export function toRelativeDate(date) {
-	moment.updateLocale('en', {
-		relativeTime: {
-			s: 'a moment',
-			m: '%d minute',
-			mm: '%d mins',
-			h: '1 hr',
-			hh: '%d hrs',
-			d: 'Yesterday',
-		},
-	});
-	return moment(date).fromNow(true);
+    moment.updateLocale('en', {
+        relativeTime: {
+            s: 'a moment',
+            m: '%d minute',
+            mm: '%d mins',
+            h: '1 hr',
+            hh: '%d hrs',
+            d: 'Yesterday',
+        },
+    });
+    return moment(date).fromNow(true) + ' ago';
 }
