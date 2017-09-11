1bbc25b6a3b5e9f4ded39f462eed91667fc1cbb3
Lucas Arundell <larundell@small-improvements.com>
2017-09-11 20:03:50 +0200

    feat(Comment) add edit/delete UI, not wired up yet

--- components/Comment/index.js
+++ components/Comment/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { Text, TouchableHighlight } from 'react-native';
+import { View, Text, TouchableHighlight, TouchableOpacity, ActionSheetIOS } from 'react-native';
 import styleVars from '../../assets/styles/vars';
 import Avatar from '../Avatar';
 import HTMLView from 'react-native-htmlview';
-const { standardPadding, commentBorder, commentBackgroundColour } = styleVars;
+import DateDisplay from '../DateDisplay';
+const { standardPadding, commentBorder, commentBackgroundColour, subduedTextColor } = styleVars;
 
 const CommentContainer = styled.View`
     border-radius: 3px;
@@ -22,34 +23,74 @@ const CommentContent = styled.View`
     margin-left: ${standardPadding * 0.5}px;
 `;
 
-const LightGrayText = styled.Text`color: gray;`;
+const LightGrayText = styled.Text`
+    color: ${subduedTextColor};
+    font-weight: bold;
+`;
 
 const CommentText = styled.View`margin-bottom: ${standardPadding * 0.5}px;`;
+const CommentDate = styled(DateDisplay)`color: ${subduedTextColor};`;
 
 export default function Comment(props) {
     const { moduleType, onPressReply } = props;
     let commentData = props;
+    let canEditMessage, canEditComment;
 
-    if (moduleType === 'MESSAGE' || moduleType === 'PRAISE') {
+    const isPraise = moduleType === 'MESSAGE' || moduleType === 'PRAISE';
+
+    if (isPraise) {
         commentData = transformPraiseCommentModel(commentData);
+        canEditMessage = commentData.permissions.canEditMessage;
+        canEditComment = commentData.permissions.canEditComment;
     }
 
-    const { body } = commentData;
+    const { body, createdAt } = commentData;
     const { logo, name } = commentData.author;
+
+    const showCommentOptions = () => {
+        ActionSheetIOS.showActionSheetWithOptions(
+            {
+                options: ['Edit', 'Delete', 'Cancel'],
+                cancelButtonIndex: 2,
+                destructiveButtonIndex: 1,
+            },
+            buttonIndex => {
+                switch (buttonIndex) {
+                    case 0:
+                        console.log('edit');
+                        break;
+                    case 1:
+                        console.log('delete');
+                        break;
+                }
+            }
+        );
+    };
+    return canEditMessage && canEditMessage ? (
+        <TouchableOpacity onLongPress={showCommentOptions}>
+            <CommentBody logo={logo} body={body} createdAt={createdAt} onPressReply={onPressReply} name={name} />
+        </TouchableOpacity>
+    ) : (
+        <CommentBody logo={logo} body={body} createdAt={createdAt} onPressReply={onPressReply} name={name} />
+    );
+}
+
+function CommentBody({ logo, body, createdAt, onPressReply, name }) {
     return (
         <CommentContainer>
             <Avatar logoUrl={logo} />
             <CommentContent>
                 <CommentText>
-                    <Text style={{ fontWeight: 'bold' }}>
-                        {name}
-                    </Text>
+                    <Text style={{ fontWeight: 'bold' }}>{name}</Text>
                     <HTMLView value={body ? body : 'Empty comment'} />
                 </CommentText>
-
-                <TouchableHighlight onPress={onPressReply}>
-                    <LightGrayText>Reply</LightGrayText>
-                </TouchableHighlight>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <CommentDate date={createdAt} />
+                    <Text> · </Text>
+                    <TouchableHighlight onPress={onPressReply}>
+                        <LightGrayText>Reply</LightGrayText>
+                    </TouchableHighlight>
+                </View>
             </CommentContent>
         </CommentContainer>
     );
