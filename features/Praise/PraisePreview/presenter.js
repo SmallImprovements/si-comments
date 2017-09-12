fe3448ba0d50ff9a183bce2156d94459d3fdf821
Lucas Arundell <larundell@small-improvements.com>
2017-09-12 16:41:21 +0200

    refactor(PraisePreview) make stateful component

--- features/Praise/PraisePreview/presenter.js
+++ features/Praise/PraisePreview/presenter.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Alert, TouchableHighlight } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Alert, TouchableHighlight, ActivityIndicator } from 'react-native';
 import Flex from '../../../components/Flex';
 import Avatar from '../../../components/Avatar';
 import Badge from '../../../components/Badge';
@@ -7,6 +7,7 @@ import { HeaderTwo } from '../../../components/Text';
 import styled from 'styled-components/native';
 import colorVars from '../../../assets/styles/colours';
 import LikeButton from '../../../components/LikeButton';
+import { getPraiseById } from '../../../services/api';
 
 const { SIGray3 } = colorVars;
 const showAlert = () =>
@@ -28,32 +29,63 @@ const StyledBadge = styled(Badge)`
     margin-top: -25px;
 `;
 
-export default function PraisePreview({ praise }) {
-    const { title, author, badge, recipients } = praise;
-    const firstRecipient = recipients[0];
-    return (
-        <TouchableHighlight onPress={showAlert}>
-            <Flex>
-                <StyledBadge badge={badge} />
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        marginLeft: -30,
-                    }}
-                >
-                    <Avatar avatarSize={60} logoUrl={firstRecipient.logo} />
-                    <Flex style={{ flexWrap: 'wrap', marginTop: 10 }}>
-                        <HeaderTwo style={{ fontWeight: 'bold', flexBasis: '100%', textAlign: 'center' }}>
-                            {title}
-                        </HeaderTwo>
-                        <Text style={{ color: SIGray3, flexBasis: '100%', textAlign: 'center' }}>
-                            Written by: {author.name}
-                        </Text>
-                        <LikeButton praise={praise} />
-                    </Flex>
-                </View>
-            </Flex>
-        </TouchableHighlight>
-    );
+export default class PraisePreview extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            praise: null,
+        };
+    }
+
+    componentDidMount() {
+        const { entityId } = this.props;
+        return {
+            praise: this.doGetPraise(entityId),
+        };
+    }
+
+    doGetPraise(entityId) {
+        this.setState({ isLoading: true });
+        return getPraiseById(entityId).then(res => {
+            this.setState({
+                praise: res,
+                isLoading: false,
+            });
+        });
+    }
+    render() {
+        const { praise, isLoading } = this.state;
+        if (!praise || isLoading) {
+            return <ActivityIndicator />;
+        }
+        const { title, author, badge, recipients } = praise;
+        const firstRecipient = recipients[0];
+
+        return (
+            <TouchableHighlight onPress={showAlert}>
+                <Flex>
+                    <StyledBadge badge={badge} />
+                    <View
+                        style={{
+                            flex: 1,
+                            alignItems: 'center',
+                            marginLeft: -30,
+                        }}
+                    >
+                        <Avatar avatarSize={60} logoUrl={firstRecipient.logo} />
+                        <Flex style={{ flexWrap: 'wrap', marginTop: 10 }}>
+                            <HeaderTwo style={{ fontWeight: 'bold', flexBasis: '100%', textAlign: 'center' }}>
+                                {title}
+                            </HeaderTwo>
+                            <Text style={{ color: SIGray3, flexBasis: '100%', textAlign: 'center' }}>
+                                Written by: {author.name}
+                            </Text>
+                            <LikeButton praise={praise} />
+                        </Flex>
+                    </View>
+                </Flex>
+            </TouchableHighlight>
+        );
+    }
 }
