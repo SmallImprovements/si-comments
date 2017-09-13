4044077113542d2c68edbb89bf8223fc2be49b81
Lucas Arundell <larundell@small-improvements.com>
2017-09-13 21:47:33 +0200

    feat(LikeButton) make generic component, pass in state as props

--- components/LikeButton/index.js
+++ components/LikeButton/index.js
@@ -1,57 +1,55 @@
 import React from 'react';
 import { Component, PropTypes } from 'react';
 import { TouchableOpacity, View, Text, ActivityIndicator } from 'react-native';
-// import * as MessageVotingService from '../../../../../../screens/message/services/message-voting/service';
-import { likePraise, unlikePraise } from '../../services/api';
+import styled from 'styled-components/native';
+import colorVars from '../../assets/styles/colours';
 
 const propTypes = {
-    praise: PropTypes.object.isRequired,
-    onPress: PropTypes.func.isRequired,
+    onLike: PropTypes.func.isRequired,
+    isLiked: PropTypes.bool.isRequired,
 };
+
+const StyledLikeButton = styled.View`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+`;
+
+const HeartIcon = styled.Image`
+    width: 15px;
+    height: 15px;
+    margin-right: 5px;
+`;
 
 class LikeButton extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            isLoading: false,
-        };
-
-        this.onVote = this.onVote.bind(this);
     }
-
-    onVote(canVoteUp, canVoteDown) {
-        if (!canVoteUp) {
-            return;
-        }
-
-        const { praise } = this.props;
-        this.setState({ isLoading: true });
-
-        const doVote = canVoteUp ? likePraise : unlikePraise;
-
-        doVote(praise.id).then(() =>
-            this.setState({
-                isLoading: false,
-            })
-        );
+    
+    componentWillMount() {
+        this.setState({ isLiked: this.props.isLiked });
     }
 
     render() {
-        const { isLoading } = this.state;
-        const { praise } = this.props;
-        const { canVoteUp, canVoteDown } = praise.permissions;
+        const { isLoading, isLiked, onLike } = this.props;
 
         return (
-            <TouchableOpacity onPress={() => this.onVote(canVoteUp, canVoteDown)}>
-                <ButtonContents isLoading={isLoading} canVoteUp={canVoteUp} />
+            <TouchableOpacity onPress={onLike}>
+                <ButtonContents isLoading={isLoading} isLiked={isLiked} />
             </TouchableOpacity>
         );
     }
 }
 
-function ButtonContents({ isLoading, canVoteUp }) {
-    return <View>{isLoading ? <ActivityIndicator /> : <Text>{canVoteUp ? 'Like' : 'Liked'}</Text>}</View>;
+function ButtonContents({ isLoading, isLiked }) {
+    return (
+        <StyledLikeButton>
+            {!isLoading && isLiked && <HeartIcon resizeMode="contain" source={require('./png/heart.png')} />}
+            {!isLoading && !isLiked && <HeartIcon resizeMode="contain" source={require('./png/heart-o.png')} />}
+            {isLoading && <ActivityIndicator color={colorVars.SIBlue} style={{ marginRight: 10 }} />}
+            <Text style={{ color: colorVars.SIBlue }}>{isLiked ? 'Liked' : 'Like'}</Text>
+        </StyledLikeButton>
+    );
 }
 
 LikeButton.propTypes = propTypes;
