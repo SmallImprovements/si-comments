ad26cc67bcfdb8efdd31d0384e8b7d4921e257d0
Lucas Arundell <larundell@small-improvements.com>
2017-09-16 18:30:53 +0200

    chore(dateDisplay) remove unused imports

--- components/DateDisplay.js
+++ components/DateDisplay.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { toRelativeDate } from '../services/date';
-// import styled from 'styled-components/native';
-// import colorVars from '../assets/styles/colours';
-// import styleVars from '../assets/styles/vars';
 
 export default function DateDisplay(props) {
     const { date } = props;
