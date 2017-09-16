36dffebf202e25904edad55da6e996edc311623a
Lucas Arundell <larundell@small-improvements.com>
2017-09-16 18:37:47 +0200

    feat(notification-templates) handle liking

--- services/notification-templates/templates.js
+++ services/notification-templates/templates.js
@@ -53,11 +53,15 @@ const TEMPLATES = {
     MESSAGE_COMMENT_FOR_AUTHOR: createMessageTemplate('commented on a :messages.singular: you wrote.'),
     MESSAGE_COMMENT_FOR_RECIPIENT: createMessageTemplate('commented on a :messages.singular: about you.'),
     MESSAGE_COMMENT_MENTIONED_YOU: createMessageTemplate('mentioned you in a comment on a :messages.singular:.'),
+    MESSAGE_OF_AUTHOR_LIKED: createMessageTemplate('liked a :messages.singular: you wrote.'),
+    MESSAGE_FOR_RECIPIENT_LIKED: createMessageTemplate('liked a :messages.singular: about you.'),
 
     PRAISE_COMMENT_FOR_AUTHOR: createPraiseTemplate('commented on a :praise.singular.big: you wrote.'),
     PRAISE_COMMENT_FOR_RECIPIENT: createPraiseTemplate('commented on a :praise.singular.big: for you.'),
     PRAISE_COMMENT_MENTIONED_YOU: createPraiseTemplate('mentioned you in a comment on a :praise.singular.big:.'),
     PRAISE_RECEIVED: createPraiseTemplate('gave you :praise.singular.big:.'),
+    PRAISE_OF_AUTHOR_LIKED: createPraiseTemplate('liked a :praise.singular.big: you wrote.'),
+    PRAISE_FOR_RECIPIENT_LIKED: createPraiseTemplate('liked a :praise.singular.big: for you.'),
 
     OBJECTIVE_COMMENT_CREATED_FOR_OWNER: {
         standard: {
