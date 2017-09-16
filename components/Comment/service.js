28e1d248410e7d752b32677e29e607c6428f50e2
Lucas Arundell <larundell@small-improvements.com>
2017-09-16 21:53:41 +0200

    refactor(Comment) prettier

--- components/Comment/service.js
+++ components/Comment/service.js
@@ -2,33 +2,33 @@ import { deletePraiseComment, deleteObjectiveComment } from '../../services/api'
 import { Alert } from 'react-native';
 
 export function doDeleteComment({ masterEntityId, id, moduleType, doGetComments }) {
-	if (moduleType === 'OBJECTIVE') {
-		return deleteObjectiveComment(masterEntityId, id).then(
-			res => handleResponse(res, doGetComments),
-			err => handleError(err)
-		);
-	}
-	return deletePraiseComment(masterEntityId, id).then(
-		res => handleResponse(res, doGetComments),
-		err => handleError(err)
-	);
+    if (moduleType === 'OBJECTIVE') {
+        return deleteObjectiveComment(masterEntityId, id).then(
+            res => handleResponse(res, doGetComments),
+            err => handleError(err)
+        );
+    }
+    return deletePraiseComment(masterEntityId, id).then(
+        res => handleResponse(res, doGetComments),
+        err => handleError(err)
+    );
 }
 
 function handleResponse(res, doGetComments) {
-	checkForError(res);
-	doGetComments();
-	return res;
+    checkForError(res);
+    doGetComments();
+    return res;
 }
 
 function handleError(err) {
-	console.log('error deleting comment', err);
+    console.log('error deleting comment', err);
 }
 
 function checkForError(res) {
-	return res.status === 'ERROR' && showErrorAlert(res.err);
+    return res.status === 'ERROR' && showErrorAlert(res.err);
 }
 function showErrorAlert(err) {
-	Alert.alert("Can't delete comment", `${err}`, [{ text: 'OK', onPress: () => console.log('OK Pressed') }], {
-		cancelable: true,
-	});
+    Alert.alert("Can't delete comment", `${err}`, [{ text: 'OK', onPress: () => console.log('OK Pressed') }], {
+        cancelable: true,
+    });
 }
