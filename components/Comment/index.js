f67f80e08cfb2702a6ffea4b4ea6429b66c983d9
Lucas Arundell <larundell@small-improvements.com>
2017-09-16 18:31:17 +0200

    feat(Comment) implement delete comment and copy text

--- components/Comment/index.js
+++ components/Comment/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/native';
-import { View, Text, TouchableHighlight, TouchableOpacity, ActionSheetIOS } from 'react-native';
+import { View, Text, TouchableHighlight, TouchableOpacity, ActionSheetIOS, Alert, Clipboard } from 'react-native';
 import styleVars from '../../assets/styles/vars';
 import Avatar from '../Avatar';
 import HTMLView from 'react-native-htmlview';
 import DateDisplay from '../DateDisplay';
+import { deletePraiseComment } from '../../services/api';
 const { standardPadding, commentBorder, commentBackgroundColour, subduedTextColor } = styleVars;
 
 const CommentContainer = styled.View`
@@ -31,8 +32,33 @@ const LightGrayText = styled.Text`
 const CommentText = styled.View`margin-bottom: ${standardPadding * 0.5}px;`;
 const CommentDate = styled(DateDisplay)`color: ${subduedTextColor};`;
 
+function doDelete(isPraise, masterMessageId, id, doGetComments) {
+    if (!isPraise) {
+        // todo: implement objective comment deletion
+        return;
+    }
+    return deletePraiseComment(masterMessageId, id).then(
+        res => {
+            const { err } = res;
+            if (res.status === 'ERROR') {
+                Alert.alert(
+                    "Can't delete comment",
+                    `${err}`,
+                    [{ text: 'OK', onPress: () => console.log('OK Pressed') }],
+                    {
+                        cancelable: true,
+                    }
+                );
+            }
+            doGetComments();
+            return res;
+        },
+        err => console.log('error deleting comment', err)
+    );
+}
+
 export default function Comment(props) {
-    const { moduleType, onPressReply } = props;
+    const { moduleType, onPressReply, doGetComments } = props;
     let commentData = props;
     let canEditMessage, canEditComment;
 
@@ -43,24 +69,31 @@ export default function Comment(props) {
         canEditMessage = commentData.permissions.canEditMessage;
         canEditComment = commentData.permissions.canEditComment;
     }
-
-    const { body, createdAt } = commentData;
+    const { body, createdAt, masterMessageId, id } = commentData;
     const { logo, name } = commentData.author;
 
     const showCommentOptions = () => {
         ActionSheetIOS.showActionSheetWithOptions(
             {
-                options: ['Edit', 'Delete', 'Cancel'],
-                cancelButtonIndex: 2,
-                destructiveButtonIndex: 1,
+                options: ['Edit', 'Copy', 'Delete', 'Cancel'],
+                cancelButtonIndex: 3,
+                destructiveButtonIndex: 2,
             },
             buttonIndex => {
                 switch (buttonIndex) {
                     case 0:
-                        console.log('edit');
+                        Alert.alert('Not yet implemented', 'You are unable to edit comments', [{ text: 'OK' }], {
+                            cancelable: true,
+                        });
                         break;
+
                     case 1:
-                        console.log('delete');
+                        Clipboard.setString(body);
+                        break;
+                    case 2:
+                        doDelete(isPraise, masterMessageId, id, doGetComments);
+                        break;
+                    default:
                         break;
                 }
             }
