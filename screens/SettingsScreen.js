0e66adda3a6c881c15ca3809e542c99fa38d8f03
Lucas Arundell <larundell@small-improvements.com>
2017-09-24 15:51:11 +0200

    feat(Settings) style

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import { ExpoConfigView } from '@expo/samples';
 import { View, Text, Button, AsyncStorage } from 'react-native';
 import auth from '../services/auth';
 import Avatar from '../components/Avatar';
 import { HeaderOne } from '../components/Text';
+import colorVars from '../assets/styles/colours';
 
+const { SIMainRed, SIGray2 } = colorVars;
 export default class SettingsScreen extends React.Component {
     static navigationOptions = {
         title: 'My Account',
@@ -27,22 +28,19 @@ export default class SettingsScreen extends React.Component {
 
     render() {
         const { currentUser } = this.props.screenProps;
-        const { name, logo } = currentUser;
+        const { name, logo, email } = currentUser;
         const { userToken } = this.state;
         const logout = () => auth.logout();
         const clearAllFromLocalDB = () => auth.clearAllFromLocalDB();
         return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <HeaderOne>
-                    Logged in as{' '}
-                    <Text>
-                        <Avatar logoUrl={logo} />
-                        {name}
-                    </Text>
-                </HeaderOne>
-                <HeaderOne style={{ color: userToken ? 'black' : 'red' }}>Token is {userToken || 'not set!'}</HeaderOne>
-                <View>
-                    <Button title="Logout" onPress={logout} />
+            <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}>
+                <View style={{ flexGrow: 1, marginTop: 50, alignItems: 'center' }}>
+                    <Avatar logoUrl={logo} avatarSize={80} />
+                    <HeaderOne style={{ marginTop: 10 }}>{name}</HeaderOne>
+                    <Text style={{ marginTop: 10, color: SIGray2 }}>{email}</Text>
+                </View>
+                <View style={{ marginBottom: 40 }}>
+                    <Button color={SIMainRed} title="Logout" onPress={logout} />
                     <Button title="Clear all Local Storage" onPress={clearAllFromLocalDB} />
                 </View>
             </View>
