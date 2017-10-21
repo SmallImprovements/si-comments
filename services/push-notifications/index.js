edadea5cb708d1855bbe4a7a50eb8c57827eb1bf
Lucas Arundell <larundell@small-improvements.com>
2017-10-22 00:19:50 +0200

    feat(push notifications) enable

--- services/push-notifications/index.js
+++ services/push-notifications/index.js
@@ -1,38 +1,31 @@
-import http from '../http';
 import { Permissions, Notifications } from 'expo';
-const PUSH_ENDPOINT = '/api/v2/notificationLogs/push-token/12345';
+import auth from '../auth';
+// const PUSH_ENDPOINT = '/api/v2/mobile-app/register-device';
+import { AsyncStorage } from 'react-native';
 
 async function registerForPushNotificationsAsync() {
-    const { existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-    let finalStatus = existingStatus;
-    // console.log('Push notifications check entered');
-    // only ask if permissions have not already been determined, because
-    // iOS won't necessarily prompt the user a second time.
-    if (existingStatus !== 'granted') {
-        // Android remote notification permissions are granted during the app
-        // install, so this will only ask on iOS
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
-    }
+  const { existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  let finalStatus = existingStatus;
 
-    // Stop here if the user did not grant permissions
-    if (finalStatus !== 'granted') {
-        return;
-    }
+  if (existingStatus !== 'granted') {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
 
-    // Get the token that uniquely identifies this device
-    let token = await Notifications.getExpoPushTokenAsync();
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
 
-    // POST the token to our backend so we can use it to send pushes from there
-    http.defaults.baseURL = 'http://192.168.1.25:8080';
-    // console.log('Device Push Notification Token: ', token);
-    return http
-        .post(PUSH_ENDPOINT, {
-            params: { token: token },
-        })
-        .then(res => {
-            // console.log('Successfully hit Push Token endppoint');
-            return res.data;
-        });
+  let deviceId = await Notifications.getExpoPushTokenAsync();
+
+  return auth
+    .registerDeviceId(deviceId)
+    .then(res => res, err => err)
+    .then(storeDeviceIdLocally(deviceId));
+}
+
+function storeDeviceIdLocally(deviceId) {
+  AsyncStorage.setItem('deviceId', deviceId).done();
 }
 export default registerForPushNotificationsAsync;
