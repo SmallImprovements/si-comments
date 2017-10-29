8128e89f385202a36a20b0d88da7dfae9d2bed78
Lucas Arundell <larundell@small-improvements.com>
2017-10-29 21:18:54 +0100

    chore(PropTypes) use new prop-types package

--- components/LikeButton/index.js
+++ components/LikeButton/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import { Component } from 'react';
 import { TouchableOpacity, View, Text, ActivityIndicator } from 'react-native';
 import styled from 'styled-components/native';
 import colorVars from '../../assets/styles/colours';
